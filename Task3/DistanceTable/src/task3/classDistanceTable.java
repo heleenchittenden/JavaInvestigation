4be77056f1b5891fed57498beafe36c0365d8694
heleenchittenden <45969065+heleenchittenden@users.noreply.github.com>
2019-01-14 09:12:06 +0200

    Update classDistanceTable.java

--- Task3/DistanceTable/src/task3/classDistanceTable.java
+++ Task3/DistanceTable/src/task3/classDistanceTable.java
@@ -9,13 +9,13 @@ import java.util.HashMap;
  *
  * @author HChittenden
  */
-public class classDistanceTable {
+public class DistanceTable {
     
     enum CitiesEnum {AMSTERDAM, BERLIN, COPENHAGEN, LONDON, MOSCOW, ROME, WARSAW;}
     
     HashMap<CitiesEnum, HashMap<CitiesEnum, Integer>> DistanceTable = new HashMap<>();
     
-    public Integer GetDistance(CitiesEnum City1, CitiesEnum City2) {
+    public Integer getDistance(CitiesEnum City1, CitiesEnum City2) {
         return DistanceTable.get(City1).get(City2);
     } 
     
